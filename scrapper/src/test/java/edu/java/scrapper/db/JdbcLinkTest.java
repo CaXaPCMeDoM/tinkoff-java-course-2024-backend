24be297495b7752677d7bce3aa54675e9df84872
Vladislav <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-03-14 18:51:12 +0300

    всё починил

--- scrapper/src/test/java/edu/java/scrapper/db/JdbcLinkTest.java
+++ scrapper/src/test/java/edu/java/scrapper/db/JdbcLinkTest.java
@@ -1,15 +1,18 @@
 package edu.java.scrapper.db;
 
+import edu.java.dao.ChatDao;
 import edu.java.dao.ChatLinkDao;
 import edu.java.dao.LinkDao;
+import edu.java.dao.dto.ChatDto;
 import edu.java.dao.dto.ChatLinkDto;
+import edu.java.dao.dto.LinkDto;
+import java.time.LocalDateTime;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
-
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest
@@ -20,6 +23,8 @@ public class JdbcLinkTest extends IntegrationEnvironment {
     private LinkDao linkDao;
     @Autowired
     private JdbcTemplate jdbcTemplate;
+    @Autowired
+    private ChatDao chatDao;
 
     private static final Long CHAT_ID = 1L;
 
@@ -27,17 +32,58 @@ public class JdbcLinkTest extends IntegrationEnvironment {
     @Transactional
     @Rollback
     void addTest() {
+        int sizeAfterAdd = chatLinkDao.findAll().size(); // нужна проверка при переполнении
+
+
+        LinkDto linkDto = new LinkDto(1L, "https://edu.tinkoff.ru", LocalDateTime.now(), LocalDateTime.now(), "testUser");
+        linkDao.add(linkDto);
+
+        ChatDto chatDto = new ChatDto(CHAT_ID);
+        chatDao.add(chatDto);
+
         Long linkId = linkDao.findAll().getFirst().getUrlId();
         ChatLinkDto chatLinkDto = new ChatLinkDto(CHAT_ID, linkId);
 
         chatLinkDao.add(chatLinkDto);
 
-        assertThat(chatLinkDao.findAll().size()).isEqualTo(1);
+        int sizeBeforeAdd = sizeAfterAdd + 1; // нужна проверка при переполнении
+
+        assertThat(chatLinkDao.findAll().size()).isEqualTo(sizeBeforeAdd);
     }
 
     @Test
     @Transactional
     @Rollback
     void removeTest() {
+        // Сначала добавим
+
+        int sizeAfterAdd = chatLinkDao.findAll().size(); // нужна проверка при переполнении
+
+
+        LinkDto linkDto = new LinkDto(1L, "https://edu.tinkoff.ru", LocalDateTime.now(), LocalDateTime.now(), "testUser");
+        linkDao.add(linkDto);
+
+        ChatDto chatDto = new ChatDto(CHAT_ID);
+        chatDao.add(chatDto);
+
+        Long linkId = linkDao.findAll().getFirst().getUrlId();
+        ChatLinkDto chatLinkDto = new ChatLinkDto(CHAT_ID, linkId);
+
+        chatLinkDao.add(chatLinkDto);
+
+        int sizeBeforeAdd = sizeAfterAdd + 1; // нужна проверка при переполнении
+
+        assertThat(chatLinkDao.findAll().size()).isEqualTo(sizeBeforeAdd);
+
+        // Переходим к удалению
+
+
+        int sizeAfterRemove = chatLinkDao.findAll().size(); // нужна проверка при переполнении
+
+        chatLinkDao.remove(CHAT_ID, linkDto.getUrlId());
+
+        int sizeBeforeRemove = sizeAfterRemove - 1; // нужна проверка при переполнении
+
+        assertThat(chatLinkDao.findAll().size()).isEqualTo(sizeBeforeRemove);
     }
 }
