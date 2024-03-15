5d11349fa96820dbe46f5b2696cf49d2a34b90f4
Vladislav <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-03-16 01:23:27 +0300

    fix: fix updateClient from scrapper

--- scrapper/src/main/java/edu/java/internal/client/dto/LinkClientUpdateRequest.java
+++ scrapper/src/main/java/edu/java/internal/client/dto/LinkClientUpdateRequest.java
@@ -2,8 +2,10 @@ package edu.java.internal.client.dto;
 
 import java.util.List;
 import lombok.Builder;
+import lombok.Getter;
 
 @Builder
+@Getter
 public class LinkClientUpdateRequest {
     private Long id;
     private String url;
