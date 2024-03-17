787f9e99bf0f1f1a91df4561d45fe1c8274fe5c5
Vladislav <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-03-17 15:04:36 +0300

    data_base(trigger): add trigger in Chat_link for delete unique url_id

--- migrations/sql/v1/chat-link_table.sql
+++ migrations/sql/v1/chat-link_table.sql
@@ -4,6 +4,19 @@ CREATE TABLE IF NOT EXISTS CHAT_LINK
     url_id          bigint,
 
     primary key (chat_id, url_id),
-    foreign key (chat_id) references CHAT(chat_id),
+    foreign key (chat_id) references CHAT(chat_id) on delete cascade,
     foreign key (url_id)  references Link(url_id) on delete cascade
 );
+
+CREATE OR REPLACE FUNCTION delete_unique_link() RETURNS trigger AS $$
+begin
+    if (SELECT COUNT(*) FROM CHAT_LINK WHERE url_id = OLD.url_id) = 0 then
+        DELETE FROM link WHERE url_id = OLD.url_id;
+    end if;
+    return null;
+end;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER delete_unique_link
+AFTER DELETE ON CHAT_LINK
+FOR EACH ROW EXECUTE PROCEDURE delete_unique_link();
