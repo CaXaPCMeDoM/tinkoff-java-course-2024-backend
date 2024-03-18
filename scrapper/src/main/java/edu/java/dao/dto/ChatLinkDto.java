a15328da7a12ad1dd9f734cc9e860f038f100bf4
Vladislav <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-03-18 20:33:11 +0300

    refactor: remove @Getter and @Setter and add @Data for Dto

--- scrapper/src/main/java/edu/java/dao/dto/ChatLinkDto.java
+++ scrapper/src/main/java/edu/java/dao/dto/ChatLinkDto.java
@@ -1,10 +1,8 @@
 package edu.java.dao.dto;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class ChatLinkDto {
     private Long chatId;
     private Long urlId;
--- scrapper/src/main/java/edu/java/dao/dto/LinkDto.java
+++ scrapper/src/main/java/edu/java/dao/dto/LinkDto.java
@@ -1,11 +1,10 @@
 package edu.java.dao.dto;
 
-import java.time.LocalDateTime;
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+import java.time.LocalDateTime;
+
+@Data
 public class LinkDto {
     private Long linkId;
     private String url;
