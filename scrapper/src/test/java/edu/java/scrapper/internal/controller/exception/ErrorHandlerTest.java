6313c5dbb2d4042bb1944b0129b894e61c784a5f
Vladislav <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-03-20 16:12:23 +0300

    try fix pom

--- scrapper/src/test/java/edu/java/scrapper/internal/controller/exception/ErrorHandlerTest.java
+++ scrapper/src/test/java/edu/java/scrapper/internal/controller/exception/ErrorHandlerTest.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @SpringBootTest
 @AutoConfigureMockMvc
@@ -15,5 +18,9 @@ public class ErrorHandlerTest {
 
     @Test
     public void testHandleNoSuchElementException() throws Exception {
+        mockMvc.perform(delete("/tg-chat/1"))
+            .andExpect(status().isNotFound())
+            .andExpect(jsonPath("$.description").value("Чат с ID 1 не найден"))
+            .andExpect(jsonPath("$.errorId").exists());
     }
 }
