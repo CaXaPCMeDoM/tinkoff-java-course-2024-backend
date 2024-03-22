7e21772d0ef5b48fff2d9eef1f8959170d8ecc60
Vladislav <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-03-22 13:56:09 +0300

    feat: JpaChatLinkService methods are implemented

--- scrapper/src/main/java/edu/java/service/jpa/JpaChatLinkService.java
+++ scrapper/src/main/java/edu/java/service/jpa/JpaChatLinkService.java
@@ -1,13 +1,20 @@
 package edu.java.service.jpa;
 
+import edu.java.repository.jpa.JpaChatLinkRepository;
 import edu.java.service.ChatLinkService;
 import java.util.List;
 import org.springframework.stereotype.Service;
 
 @Service
 public class JpaChatLinkService implements ChatLinkService {
+    private final JpaChatLinkRepository jpaChatLinkRepository;
+
+    public JpaChatLinkService(JpaChatLinkRepository jpaChatLinkRepository) {
+        this.jpaChatLinkRepository = jpaChatLinkRepository;
+    }
+
     @Override
     public List<Long> getChatIdsByUrlId(Long urlId) {
-        return null;
+        return jpaChatLinkRepository.findAllByChatLinkId_Link_LinkId(urlId);
     }
 }
