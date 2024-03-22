0f6f90512f68677030ca5e032cf962f5ccd9ae7d
Vladislav <114598862+CaXaPCMeDoM@users.noreply.github.com>
2024-03-22 13:55:28 +0300

    feat: add method from JpaChatLinkRepository

--- scrapper/src/main/java/edu/java/repository/jpa/JpaChatLinkRepository.java
+++ scrapper/src/main/java/edu/java/repository/jpa/JpaChatLinkRepository.java
@@ -1,9 +1,13 @@
 package edu.java.repository.jpa;
 
 import edu.java.dto.jpa.JpaChatLinkDto;
+import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface JpaChatLinkRepository extends JpaRepository<JpaChatLinkDto, Long> {
+    List<Long> findAllByChatLinkId_Link_LinkId(Long chatLinkId_link_linkId);
+//    @Query("SELECT c.chatLinkId.Chat.chatId FROM JpaChatLinkDto c WHERE c.chatLinkId.Link.linkId = :linkId")
+//    List<Long> findChatIdsByLinkId(@Param("linkId") Long linkId);
 }
